408b8764a66692fba8e60e80390dc697921cc7b0
012huang <huangweiyi_2006@qq.com>
2019-12-26 07:26:57 +0800

    由导航守卫, 拦截未登录不允许访问其他页面

--- src/router/index.js
+++ src/router/index.js
@@ -15,4 +15,22 @@ const router = new VueRouter({
   routes
 })
 
+// 挂载路由导航守卫, 拦截未登录不允许访问其他页面
+router.beforeEach((to, from, next) => {
+  // to表示将要访问的页面
+  // from表示从哪个页面跳转而来
+  // next是一个函数, 表示放行, 其中 next()是放行, next('/xxx')是强制跳转
+
+  // 如果访问的是登录页面, 之间放行让其访问
+  if (to.path === '/login') return next()
+  // 访问其他页面时, 先判断是否有token
+  const tokenStr = window.sessionStorage.getItem('token')
+
+  // 如果未登录, 强制跳转到登录页
+  if (!tokenStr) return next('/login')
+
+  // 有token, 则直接放行
+  next()
+})
+
 export default router
