dd8f8ccb4c38eabe8fd8336357cffc033da03a4d
012huang <huangweiyi_2006@qq.com>
2019-12-26 07:35:19 +0800

    退出登录功能实现

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,17 @@
 <template>
-  <div>Home组件</div>
+  <div>
+    <el-button type='info' @click='logout'>退出登录</el-button>
+  </div>
 </template>
 <script>
 export default {
+  methods: {
+    logout () {
+      // 清除token, 并跳转到登录页
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    }
+  }
 }
 </script>
 <style lang="less" scoped>
