dfdbe10a85809dccfee12b75059a0bbecab9e8b9
012huang <huangweiyi_2006@qq.com>
2019-12-26 23:03:33 +0800

    菜单数据填充

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -14,16 +14,16 @@
       <el-aside width="200px">
         <el-menu background-color="#333744" text-color="#fff" active-text-color="#ffd04b">
           <!--一级菜单 -->
-          <el-submenu index="1">
+          <el-submenu :index="item.id + ''" v-for="item in menuList" :key="item.id">
             <template slot="title">
               <i class="el-icon-location"></i>
-              <span>导航一</span>
+              <span>{{item.authName}}</span>
             </template>
             <!-- 二级菜单 -->
-            <el-menu-item index="1-1">
+            <el-menu-item :index="subItem.id + ''" v-for="subItem in item.children" :key="subItem.id">
               <template slot="title">
                 <i class="el-icon-location"></i>
-                <span>导航一</span>
+                <span>{{subItem.authName}}</span>
               </template>
             </el-menu-item>
           </el-submenu>
