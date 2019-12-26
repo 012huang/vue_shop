50b822788717c19d055eb1e2854f5c30b055a854
012huang <huangweiyi_2006@qq.com>
2019-12-26 22:38:53 +0800

    为所有请求头添加Token验证的Authorization字段

--- src/main.js
+++ src/main.js
@@ -8,6 +8,12 @@ import './assets/fonts/iconfont.css'
 import axios from 'axios'
 
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+axios.interceptors.request.use(config => {
+  console.log(config)
+  // 为所有请求头添加Token验证的Authorization字段
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
 
 Vue.prototype.$http = axios
 
