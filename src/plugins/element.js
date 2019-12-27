7fb6bb5a425336ac7cc13b158966c12253171874
012huang <huangweiyi_2006@qq.com>
2019-12-27 08:26:26 +0800

    用户列表功能-添加面包屑导航和卡片

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,5 +1,23 @@
 import Vue from 'vue'
-import { Button, Form, FormItem, Input, Message, Container, Header, Aside, Main, Menu, MenuItem, Submenu } from 'element-ui'
+import {
+  Button,
+  Form,
+  FormItem,
+  Input,
+  Message,
+  Container,
+  Header,
+  Aside,
+  Main,
+  Menu,
+  MenuItem,
+  Submenu,
+  Breadcrumb,
+  BreadcrumbItem,
+  Card,
+  Row,
+  Col
+} from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
@@ -12,5 +30,10 @@ Vue.use(Main)
 Vue.use(Menu)
 Vue.use(MenuItem)
 Vue.use(Submenu)
+Vue.use(Breadcrumb)
+Vue.use(BreadcrumbItem)
+Vue.use(Card)
+Vue.use(Row)
+Vue.use(Col)
 
 Vue.prototype.$message = Message
