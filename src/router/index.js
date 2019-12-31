7735ffc1e46c30c5e65dc413bbd2652874e76123
012huang <huangweiyi_2006@qq.com>
2019-12-31 08:15:33 +0800

    添加角色组件

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import Home from '../components/Home.vue'
 import Welcome from '../components/Welcome.vue'
 import Users from '../components/user/Users.vue'
 import Permission from '../components/power/Permission.vue'
+import Roles from '../components/power/Roles.vue'
 
 Vue.use(VueRouter)
 
@@ -18,7 +19,8 @@ const routes = [
     children: [
       { path: '/welcome', component: Welcome },
       { path: '/users', component: Users },
-      { path: '/rights', component: Permission }
+      { path: '/rights', component: Permission },
+      { path: '/roles', component: Roles }
     ]
   }
 ]
