50ce42f47a1c6c5ac5f470f71dce842effce0831
012huang <huangweiyi_2006@qq.com>
2020-01-02 07:54:30 +0800

    角色列表布局和获取角色列表数据

--- src/components/power/Roles.vue
+++ src/components/power/Roles.vue
@@ -1,8 +1,42 @@
 <template>
-  <div>角色列表</div>
+  <div>
+    <!-- 面包屑导航 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+      <el-breadcrumb-item>角色列表</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <!-- 卡片区域 -->
+    <el-card>
+      <el-row>
+        <el-column>
+          <el-button type="primary">添加权限</el-button>
+        </el-column>
+      </el-row>
+    </el-card>
+  </div>
 </template>
 <script>
 export default {
+  data() {
+    return {
+      roleList: []
+    }
+  },
+  created() {
+    this.getRoleList()
+  },
+  methods: {
+    async getRoleList() {
+      const { data: res } = await this.$http.get('roles')
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取角色列表失败')
+      }
+      this.roleList = res.data
+      console.log(this.roleList)
+    }
+  }
 }
 </script>
 <style lang="less" scoped>
